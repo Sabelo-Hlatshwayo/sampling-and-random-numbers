7c6e3401445b21ae7a16a5efa62c6e74ab256d9c
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-05-10 10:28:17 +0200

    Add the solution for Exercise 3

--- Sampling-And-Random-Numbers.R
+++ Sampling-And-Random-Numbers.R
@@ -6,3 +6,7 @@ expand.grid(1:6);
 # Determine the sample space for two rolls of a 4-sided die (or rolling two 4-sided dice simultaneously)
 expand.grid(1:4, 1:4);
 
+
+# Exercise 3
+# Determine the sample space for a single roll of a 6-sided die followed by a single flip of a coin
+expand.grid(1:6, c("H","T"));

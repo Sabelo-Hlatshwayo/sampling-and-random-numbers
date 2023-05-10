ee473d4080ac7643607f171fd3272d76acb6c428
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-05-10 10:44:04 +0200

    Add the solution for Exercise 4

--- Sampling-And-Random-Numbers.R
+++ Sampling-And-Random-Numbers.R
@@ -10,3 +10,9 @@ expand.grid(1:4, 1:4);
 # Exercise 3
 # Determine the sample space for a single roll of a 6-sided die followed by a single flip of a coin
 expand.grid(1:6, c("H","T"));
+
+
+# Exercise 4
+# A developer of a new subdivision offers a prospective home buyer a choice of 4 designs (A,B,C,D), and a garage or a carport. 
+# How many different plans are available to the buyer?
+paste("There are", 4 * 2, "different plans available to the buyer");
